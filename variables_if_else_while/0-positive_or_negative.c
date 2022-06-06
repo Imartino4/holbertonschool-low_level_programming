85d664d9b6e045719c9140922a245a3604bfd942
Imartino4 <ignacio.martacos@gmail.com>
2022-06-06 10:52:50 -0700

    Task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
  * main - random number
  * Return: 0 - Always
  */
 int main(void)
 {
+	int n;
 	srand(time(0));
-	int n = rand();
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
