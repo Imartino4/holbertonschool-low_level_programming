ccb31cce55d9902cac1400ce506c2dd413af2a61
Imartino4 <ignacio.martacos@gmail.com>
2022-06-06 15:46:10 -0700

    Task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print numbers from 0 to 9
+ * Return: 0 - Always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d",n);
+	}
+	putchar('\n');
+return (0);
+}
