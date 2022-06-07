6c000253d46e2bed6507b6da00dff7361d604133
Imartino4 <ignacio.martacos@gmail.com>
2022-06-06 18:25:07 -0700

    Task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print 0, 1, ... 9 using putchar and without using char variables
+ * Return: 0 - Always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n <57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
