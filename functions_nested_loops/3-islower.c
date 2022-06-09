8b288a013f7436cc5045a6282a303cdc819e8f8e
Imartino4 <ignacio.martacos@gmail.com>
2022-06-09 05:56:41 -0700

    Task 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,16 +2,18 @@
 #include "main.h"
 /**
  * _ilsower - return 1 if the input is a lowercase and 0 otherwise.
+ * @c: int to be checked
+ *
+ * Description: _islower evaluate c if is a lowercase or not
  * Return: 1 or 0.
  */
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122 )
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
-	else	
+	else
 		return (0);
-	
-}
 
+}
