23caab06e40ce8e030859005997fbfdc991c579f
Imartino4 <ignacio.martacos@gmail.com>
2022-06-09 07:16:42 -0700

    Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Return the absolute value
+ * @n: int
+ *
+ * Description: _abs evaluate n and returns the absolute value
+ * Return: Absolute number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return(n*-1);
+	}
+	else
+		return(n);
+}
