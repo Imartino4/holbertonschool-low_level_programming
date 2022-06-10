4b9a0a5a62ea0a1d42b3d5355cc24acc72867e80
Imartino4 <ignacio.martacos@gmail.com>
2022-06-10 12:09:04 -0700

    Task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a trinagle with #
+ * @n: int
+ *
+ * Description - print a triangle, size of n
+ * Return: void
+ */
+void print_triangle(int n)
+{
+	int i;
+	int j;
+	int k;
+
+	if ( n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i < n; i++)
+		{
+			for (j = 1; j < (n - i); j++)
+			{
+				_putchar(32);
+			}
+			for (k = (i - n); k <= n; k++)
+			{
+				_putchar('#');
+			}
+			-putchar('\n');
+		}
+	}
+}
