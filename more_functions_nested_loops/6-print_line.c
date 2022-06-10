a55f9ff25497722cc33cd51c542725164161bf61
Imartino4 <ignacio.martacos@gmail.com>
2022-06-10 10:46:36 -0700

    Task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - receives an int n and print n lines
+ * @n: int
+ *
+ * Description - print n lines
+ * Return: void
+ */
+void print_lines(int n)
+{
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
