1a2d8ec63144cd315dfea74e374bacb1863bf9ea
Imartino4 <ignacio.martacos@gmail.com>
2022-06-10 10:39:12 -0700

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -17,15 +17,11 @@ void more_numbers(void)
 		{
 			if (i >= 10)
 			{
-				_putchar((1 / 10) + '0');
-				_putchar((1 % 10) + '0');
+				_putchar((i / 10) + '0');
 			}
-			else
-			{
-				_putchar(i + '0');
-			}
+
+				_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
