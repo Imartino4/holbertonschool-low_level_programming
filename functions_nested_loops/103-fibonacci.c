7148a25b4d6fd3137830eeeed0e1f9cf48a5878c
Imartino4 <ignacio.martacos@gmail.com>
2022-06-11 08:22:38 -0700

    Advanced 15

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -8,14 +8,20 @@ int main(void)
 	int i;
 	long int n1 = 1;
 	long int n2 = 2;
+	long int fibpar = 2;
 	long int Fib;
 
-	for (i = 3; Fib <= 4000000; i++)
+	for (i = 3; i <= 4000000;)
 	{
 		Fib = n1 + n2;
 		n1 = n2;
 		n2 = Fib;
+		if ((Fib % 2 == 0) && (Fib < 4000000))
+		{
+			fibpar = fibpar + Fib;
+		}
+		i = Fib;
 	}
-	printf("%ld\n", Fib);
+	printf("%ld\n", fibpar);
 	return (0);
 }
