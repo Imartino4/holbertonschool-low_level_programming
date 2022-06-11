71af7c66985de3039e61d32ed2b59c75dcc69417
Imartino4 <ignacio.martacos@gmail.com>
2022-06-10 19:28:29 -0700

    Advanced 16

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,26 @@
 int main(void)
 {
 	int i;
-	long double n1 = 1;
-	long double n2 = 2;
 	long double fib;
 
+	long double n1 = 1;
+	long double n2 = 2;
+
 	printf("1, 2, ");
+
 	for (i = 1; i <= 96; i++)
 	{
-		fib = n2 + n1;
+		fib = n1 + n2 ;
 		n1 = n2;
 		n2 = fib;
-		printf("%.0Lf, ", fib);
+		if (n < 96)
+		{
+			printf("%.0Lf, ", fib);
+		}
+		else
+		{
+			printf("%.0LF", fib);
+		}
 	}
 	printf("\n");
 	return (0);
