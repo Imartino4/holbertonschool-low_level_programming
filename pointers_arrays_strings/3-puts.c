f33e22b296c7a4aed8035191b4a9be8813e811a5
Imartino4 <ignacio.martacos@gmail.com>
2022-06-14 11:50:29 -0700

    Task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: char
+ *
+ * Description -
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+char _putchar(char);
+void _puts(char *str);
