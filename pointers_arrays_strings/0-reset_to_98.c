d32d4bed29a2696816472ef5276f7b0e9cdcdf71
Imartino4 <ignacio.martacos@gmail.com>
2022-06-14 09:14:36 -0700

    Task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a parameterusing pointer
+ * @*n: - int pointer
+ *
+ * Description - update the value of a parameter
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
