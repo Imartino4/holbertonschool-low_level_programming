0ee11a8f00a6017522d6c9e68e61a56f1c531631
Imartino4 <ignacio.martacos@gmail.com>
2022-06-14 11:26:51 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return de length of a string
+ * @s: char
+ *
+ * Description -
+ * Return: 0
+ */
+int _strlen(char *s) /*La entrada es lo que esta adentro de s(el string*/
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (i);
+}
