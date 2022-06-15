c5596a2857479dc82aced5cbab4f4e8b6d48c87a
Imartino4 <ignacio.martacos@gmail.com>
2022-06-14 18:38:04 -0700

    Task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,7 +3,8 @@
  * rev_string - reverse a string
  * @s: char
  *
- * Description - reverse a string, el -1 es para no copiar el valor nulo y que siga quedando al final
+ * Description - reverse a string,
+ * el -1 es para no copiar el valor nulo y que siga quedando al final
  * Return: void
  */
 void rev_string(char *s)
@@ -15,7 +16,7 @@ void rev_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; j <= (i/2); j++)
+	for (j = 0; j < (i / 2); j++)
 	{
 		aux = s[j];
 		s[j] = s[i - j - 1];
