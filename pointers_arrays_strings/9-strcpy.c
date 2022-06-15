256194551d2c7932b930a8da579dcf7ec9712de7
Imartino4 <ignacio.martacos@gmail.com>
2022-06-15 11:26:59 -0700

    Task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcpy - returns the pointer to dest
+ * @dest: char
+ * @src: char
+ *
+ * Description - return the pointer to dest
+ * Return: char 
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (src);
+}
