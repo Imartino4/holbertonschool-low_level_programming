ae13aea5f77c50ddef45671ceb52eeba18d56299
Imarino4 <ignacio.martacos@gmail.com>
2022-06-16 11:07:39 -0700

    Task 4.2

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the content of an array of integers
+ * @a: int
+ * @n: int
+ *
+ * Description - 
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, aux;
+
+	for (i = 0; i = n / 2; i++)
+	{
+		aux = a[i];
+		a[i] = a[n - i];
+		a[n - i] = aux;
+	}
+}
