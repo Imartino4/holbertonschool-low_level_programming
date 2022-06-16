1bad43bc07bebd313861a0c66e08b4408a03d7d9
Imarino4 <ignacio.martacos@gmail.com>
2022-06-16 10:41:08 -0700

    Task 3.2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,23 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, r;
+	int i, j, n, r;
 
 	for (i = 0; s2[i] != '\0'; i++)
 		;
 	for (j = 0; s1[j] != '\0'; j++)
 		;
-	if (j > i)
-		r = 15;
-	else if (j == i)
-		r = 0;
-	else if (j < i)
-		r = -15;
+	if (j >= i)
+		n = i;
+	else if (i > j)
+		n = j;
+	i = 0;
+	r = 0;
+	for (i = 0; i <= n; i++)
+	{
+		if (s1[i] != s2[i])
+			break;
+	}
+	r = s1[i] - s2[i];
 	return (r);
 }
