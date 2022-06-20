5fabde1be8990e4de85c42c912169ea58a9fc1ec
Imarino4 <ignacio.martacos@gmail.com>
2022-06-20 06:13:49 -0700

    Task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse recursively
+ * @s: char
+ *
+ * Description -
+ * Return_ void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+/** Primero hace la recursion, cuando llega al nulo para y ejecuta todos 
+ * los putchar generados
+ * Si el string viene sin salto de linea no logre poner un unico salto
+ */
