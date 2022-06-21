ce844c7a4016022152d13489534f7aa9d54622c1
Imarino4 <ignacio.martacos@gmail.com>
2022-06-21 06:24:00 -0700

    Task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main -
- * @argc: number of arg
+ * @argc: int
  * @argv: array
  *
  * Return: int
  */
 int main(int argc, char *argv[])
 {
+	int n = argc;
 	printf("%s\n", argv[0]);
-		return (0);
+		return (n);
 }
