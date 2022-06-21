9fe364fe750d51b392fec3bce145f975f42c9d05
Imarino4 <ignacio.martacos@gmail.com>
2022-06-21 07:25:03 -0700

    Task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies the arguments
+ * @argc: number of arguments
+ * @arv: arrays with the arguments
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
