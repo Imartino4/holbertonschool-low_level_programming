cba922d32b7cad0e809965be2e5645b54b5cad3b
Imarino4 <ignacio.martacos@gmail.com>
2022-06-24 08:37:04 -0700

    Task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - free 2 dimensional grid
+ * @grid: pointer to pinter int
+ * @height: int
+ *
+ * Description -
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i <= height; i++)
+		free(grid[i]);
+	free(grid);
+}
