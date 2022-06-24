e43f6bbb4883e129868669aad1ee7f49bfdc3ebc
Imarino4 <ignacio.martacos@gmail.com>
2022-06-24 08:43:29 -0700

    Task 4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -13,7 +13,10 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i <= height; i++)
-		free(grid[i]);
-	free(grid);
+	if (grid != NULL)
+	{
+		for (i = 0; i <= height; i++)
+			free(grid[i]);
+		free(grid);
+	}
 }
