0d5e63c4688c2cfe0f069c10d7b0e40a986dd6c9
Imarino4 <ignacio.martacos@gmail.com>
2022-06-25 08:53:01 -0700

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	m = sizeof(s2);
 	if (n < m)
 		m = n;
-	t = malloc(sizeof(s1) + sizeof(s2) + 1);
+	t = malloc(sizeof(s1) + sizeof(s2) - 1);
 	if (t == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
