831d5392aad497e0d84b7f503db2709255e8f218
Imarino4 <ignacio.martacos@gmail.com>
2022-06-25 18:54:36 -0700

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,13 +20,30 @@ char *str_concat(char *s1, char *s2)
 	if (s2)
 		len2 = strlen(s2);
 	t = malloc(len1 + len2 + 1);
-	if (t != NULL)
+	if (s1 == NULL && s2 == NULL)
+	{
+		t[0] = '\0';
+		return (t);
+	}
+	if (s1 && s2 == NULL)
+	{
+		for (i = 0; i < len1; i++)
+			t[i] = s1[i];
+		t[i] = '\0';
+	}
+	if (s2 && s1 == NULL)
+	{
+		for (i = 0; i < len2; i++)
+			t[i] = s2[i];
+		t[i] = '\0';
+	}
+	else
 	{
 		for (i = 0; i < len1; i++)
 			t[i] = s1[i];
 		for (j = i; s2[j - i]; j++)
 			t[j] = s2[j - i];
 		t[j] = '\0';
-	}
 	return (t);
+	}
 }
