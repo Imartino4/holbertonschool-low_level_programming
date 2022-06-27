67a868037c27428d2b301c89e4a28cbd01f0d353
Imarino4 <ignacio.martacos@gmail.com>
2022-06-27 16:00:42 -0700

    Task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef Dog_structure
 #define Dog_structure
-
+/**
+ * struct dog - data of dogs
+ * @name: pointer to string of name
+ * @age: age of dog
+ * @owner: pointer to string of owner
+ *
+ * Description:
+ */
 struct dog
 {
 	char *name;
