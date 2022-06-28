1004f622517821340f08cb043163d3ff39ef17f8
Imarino4 <ignacio.martacos@gmail.com>
2022-06-27 18:58:31 -0700

    Task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,15 +9,19 @@
  */
 void print_dog(struct dog *d)
 {
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
 	if (d)
 	{
-		if ((*d).name == NULL)
-			printf("Name: (nil)\nAge: %f\nOwner: %s\n", (*d).age, (*d).owner);
-		if ((*d).age == 0)
-			printf("Name: %s\nAge: (nil)\nOwner: %s\n", (*d).name, (*d).owner);
-		if ((*d).owner == NULL)
-			printf("Name: %s\nAge: %f\nOwner: (nil)\n", (*d).name, (*d).age);
-		else
-			printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+		/*if ((*d).name == NULL)*/
+		/*	printf("Name: (nil)\nAge: %f\nOwner: %s\n", (*d).age, (*d).owner);*/
+		/*if ((*d).age == 0)*/
+		/*	printf("Name: %s\nAge: (nil)\nOwner: %s\n", (*d).name, (*d).owner);*/
+		/*if ((*d).owner == NULL)*/
+		/*	printf("Name: %s\nAge: %f\nOwner: (nil)\n", (*d).name, (*d).age);*/
+		/*else*/
+		printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 	}
 }
