8644c0ceb23bd4a18929eb825e1e612da21ae501
Imarino4 <ignacio.martacos@gmail.com>
2022-06-27 18:14:15 -0700

    Task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -5,13 +5,17 @@
  *@d: pointer to struct dog
  *@name: pointer to name
  *@age: dog's age
+ *@owner: dog's owner pointer to string
  *
  * Description -
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
