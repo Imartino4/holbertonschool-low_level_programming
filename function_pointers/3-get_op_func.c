9a98554aab095962dad2a1b451d38da3b071d2fb
Imarino4 <ignacio.martacos@gmail.com>
2022-06-29 11:48:24 -0700

    Task 3 Calculator

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -8,23 +8,23 @@
  * Description:
  * Return: pointer to function
  */
-op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-};
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op)
 	{
-		if (ops[i].op == s)
-			return (ops[i].f);
+		if (*ops[i].op == *s)
+			return(*ops[i].f);
 		i++;
 	}
-	return (NULL);
+	return(*ops[6].f);
 }
