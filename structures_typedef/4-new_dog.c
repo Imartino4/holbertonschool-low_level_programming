d28dd64b8ed9f56206f9da1456d29fa13a5c09d5
Imarino4 <ignacio.martacos@gmail.com>
2022-06-29 09:40:45 -0700

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,50 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * new_dog - creates a new variable
- * @name: pointer to string of a elementent's structure
- * @age: float
- * @owner: pointer to string of a element's structure
- *
- * Description -
- * Return: pointer to a structure.
- */
-char *_strdup(char *str);
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *point;
-	char *pname;
-	char *powner;
-	int len1 = 0, len2 = 0;
-
-	if (name)
-		len1 = strlen(name);
-	if (owner)
-		len2 = strlen(owner);
-	point = malloc(sizeof(dog_t));
-	if (point == NULL)
-		return (NULL);
-	pname = malloc(len1);
-	if (pname == NULL)
-		return (NULL);
-	pname = _strdup(name);
-	powner = malloc(len2);
-	if (powner == NULL)
-	{
-		free(powner);
-		free(pname);
-		return (NULL);
-	}
-	free(powner);
-	free(pname);
-	powner = _strdup(owner);
-	point->name = name;
-	point->age = age;
-	point->owner = owner;
-	free(point);
-	return (point);
-}
-/**
  * _strdup - return a pointer no a newly allocated space in memory
  * @str: pointer to string to copy
  *
@@ -74,4 +30,42 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
+}
+/**
+ * new_dog - creates a new variable
+ * @name: pointer to string of a elementent's structure
+ * @age: float
+ * @owner: pointer to string of a element's structure
+ *
+ * Description -
+ * Return: pointer to a structure.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *point;
+	char *pname;
+	char *powner;
+	int len1 = 0, len2 = 0;
+
+	len1 = strlen(name);
+	len2 = strlen(owner);
+	point = malloc(sizeof(dog_t));
+	if (point == NULL)
+		return (NULL);
+	point->name = name;
+	point->age = age;
+	point->owner = owner;
+	pname = malloc(len1);
+	if (pname == NULL)
+		return (NULL);
+	pname = _strdup(name);
+	powner = malloc(len2);
+	if (powner == NULL)
+	{
+		free(powner);
+		return (NULL);
+	}
+	powner = _strdup(owner);
+	return (point);
+	free(point);
 }
