936c24a671dd960cc218ef715fc27b1d9e357e88
Imarino4 <ignacio.martacos@gmail.com>
2022-06-29 12:26:14 -0700

    Task 3 Calculator

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -7,11 +7,11 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 /**
- *  * struct op - Struct op
- *   *
- *    * @op: The operator
- *     * @f: The function associated
- *      */
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+
 typedef struct op
 {
 	char *op;
