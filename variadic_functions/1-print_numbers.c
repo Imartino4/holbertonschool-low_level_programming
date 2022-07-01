fb9ddd9314389f79c8d3320d75ff541a3ab0e39c
Imarino4 <ignacio.martacos@gmail.com>
2022-07-01 06:18:04 -0700

    Task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_numbers - print numbers entered by parameters
+ * @separator: pointer to char used to separate the numbers
+ * @n: quantity of parameters-
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list pn;
+	int value = 0;
+	unsigned int i;
+
+	va_start(pn, n);
+
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(pn, int);
+		if (i < n - 1)
+			printf("%d%s", value, separator);
+		if (i >= n - 1)
+			printf("%d\n", value);
+	}
+}
