04abc0b196dfe134f007af14d018f5b02c9c089b
Imarino4 <ignacio.martacos@gmail.com>
2022-07-03 13:43:34 -0700

    Task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,7 +17,7 @@ void print_all(const char * const format, ...)
 	va_start(ptr, NULL);
 	while (format[i] && format != NULL)/*Agrego && format por el caso nulo*/
 		i++;
-	while (format[j] && format != NULL
+	while (format[j] && format != NULL)
 	{
 		if (j == (i - 1))
 			sep = "";
