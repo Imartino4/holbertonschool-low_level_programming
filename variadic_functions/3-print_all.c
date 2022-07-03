cfd50caf708ae567b55c82879b7de8620b23e886
Imarino4 <ignacio.martacos@gmail.com>
2022-07-03 13:10:33 -0700

    Task 3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ptr;
+	int i = 0, j = 0;
+	char *sep = ", ";/*Lo defino por la limitante de los if*/
+	char *str;
+
+	va_start(ptr, NULL);
+	while (format[i])
+		i++;
+	while (format[j])
+	{
+		if (j == (i - 1))
+			sep = "";
+		switch (format[j])
+		{
+			case 'c':/*El char pasa a ser int-ver-*/
+				printf("%c%s", va_arg(ptr, int), sep);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(ptr, int), sep);
+				break;
+			case 'f':/*Float pasa a double*/
+				printf("%f%s", va_arg(ptr, double), sep);
+				break;
+			case 's':
+				str = va_arg(ptr, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", str, sep);
+				break;
+		}
+		j++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
