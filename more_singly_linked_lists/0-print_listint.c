7b522aaa161bc30ec72852c5a605e893e69f64ca
Imarino4 <ignacio.martacos@gmail.com>
2022-07-15 09:00:36 -0700

    Task 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - print all the elements of a list
+ * @h: pointer to first element of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_ t count;
+
+	if (h)
+	{
+		for (count = 0; h != NULL; count++)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+	}
+	return (count)
+}
