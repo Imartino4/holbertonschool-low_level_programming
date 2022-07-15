9b2bc9fae53ff0d3069b561d58582b3c486dc4e6
Imarino4 <ignacio.martacos@gmail.com>
2022-07-15 12:02:18 -0700

    Task 3

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * add_nodeint_end - adds a mew node at the end of a list
+ * @head: pointer to pointer to the first element of a list
+ * @n: data to introduce in node
+ * Return: pointer to new element if exist, otherwise NULL
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new = NULL;
+	listint_t *aux;
+
+	new = malloc(sizeof(listint_t));
+	new-n = n;
+	new->next = NULL;
+
+	if (*head)
+	{
+		aux = *head
+		while (aux->next != NULL)
+			aux = aux->next;
+		aux->next = new;
+	}
+	else
+		*head = new;
+	return (new);
+}
