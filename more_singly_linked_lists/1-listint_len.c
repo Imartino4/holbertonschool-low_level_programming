4055264b9ea183f341fc035bb49b5da4425c3548
Imarino4 <ignacio.martacos@gmail.com>
2022-07-15 10:47:30 -0700

    Task 1

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - return number of elements in a linked list
+ * @h: pointer to first element
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h)
+	{
+		for (count = 0; h; count++)
+			h=h->next;
+	}
+	return (count);
+}
