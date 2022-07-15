a2201198a73f8d4035136975dfc05febb6955531
Imarino4 <ignacio.martacos@gmail.com>
2022-07-15 14:28:37 -0700

    Task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	while (aux && cont != (idx - 1))
+	while (aux && count != (idx - 1))
 	{
 		count++;
 		aux = aux->next;
