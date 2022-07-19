f9d7d67ed58f8186089bde796222c78f00231fe4
Imarino4 <ignacio.martacos@gmail.com>
2022-07-19 16:37:50 -0700

    Task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and print it to SO
+ * @filename: file to be readed and printed
+ * @letters: number of letter to read and print
+ * Return: Actual number of letters printed, 0 otherwhise.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = 0;
+	char buf[letters];
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+	read = read(fd, buffer, letters);
+	close
+	write = write(STDOUT_FILENO, buffer, read);
+
+	if (read == -1 || write == -1)
+		return (0);
+	close (fd);
+	return (write);
+}
