eb36c4929ed1f3fde896f9f62fd9061e7356fa5f
Imarino4 <ignacio.martacos@gmail.com>
2022-07-20 16:56:02 -0700

    Task 3 updated

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,15 +12,15 @@ int main(int argc, char **argv)
 
 	if (argc != 3)/*Number of argument incorrect*/
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	/*if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}
+	}*/
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((r = read(fd_from, buffer, 1024)) > 0)
 		if (write(fd_to, buffer, r) != r || fd_to == -1)
@@ -28,6 +28,11 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
+	if (r < 0 || fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
