8b5098e2f79fc3b09739efbbaf8cb40b3178c910
Imarino4 <ignacio.martacos@gmail.com>
2022-07-19 18:02:51 -0700

    Task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - create a file
+ * @filename: file to be created
+ * @text_content: content of file
+ * Return: 1 on succes, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fd_w;
+	
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	/*0600 equal to rw------- permissions*/
+	if (fd == -1)
+		return (-1);
+
+	fd_w = write(fd, text_content, strlen(text_content));
+	if (fd_w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
