fb82af352e6498ef68aa5c1feca7b97be2c82057
Imarino4 <ignacio.martacos@gmail.com>
2022-07-20 16:57:29 -0700

    Task 3 updated

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,11 +16,6 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
-	/*if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}*/
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((r = read(fd_from, buffer, 1024)) > 0)
 		if (write(fd_to, buffer, r) != r || fd_to == -1)
