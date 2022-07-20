0bcd3677c70b4b4fb0ef8acd4155b1ba1a8cf5d0
Imarino4 <ignacio.martacos@gmail.com>
2022-07-19 18:07:46 -0700

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		return (1) /*Empty file*/
+		return (1); /*Empty file*/
 	fd_w = write(fd, text_content, strlen(text_content));
 	if (fd_w == -1)
 		return (-1);
