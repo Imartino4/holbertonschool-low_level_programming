c6097af96acd902c1eabc8fab45f4370991e37a7
Imarino4 <ignacio.martacos@gmail.com>
2022-07-21 20:14:17 -0700

    Task 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *aux = head;
 	unsigned int count = 0;
 
-	while (aux->next != NULL && count < index)
+	while (aux->next != NULL && count != index)
 	{
 		count++;
 		aux = aux->next;
