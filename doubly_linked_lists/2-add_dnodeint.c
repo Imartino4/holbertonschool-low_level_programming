d8649faa1e2096165104c3a0785d4911ab3e2e41
Imarino4 <ignacio.martacos@gmail.com>
2022-07-22 13:35:59 -0700

    Task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,8 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
+	if (new_node == NULL)
+		return (NULL);
 	new_node->prev = NULL;
 	new_node->next = *head;
 	new_node->n = n;
