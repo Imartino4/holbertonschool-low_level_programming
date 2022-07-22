0fd62f09f361d956d5bf0a19a5a4f5df630cc5a8
Imarino4 <ignacio.martacos@gmail.com>
2022-07-21 19:01:07 -0700

    Task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to first element of a list
+ * Return: void
+ */
+free_dlistint(dlistint_t *head)
+{
+	dlistint_t *aux = head;
+
+	while (aux->next)
+	{
+		head = aux->next;
+		free(aux);
+		aux = head;
+	}
+}
