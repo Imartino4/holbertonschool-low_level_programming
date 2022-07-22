ee6879c91e8f4c3808dbe16d434f7618c50e0bcc
Imarino4 <ignacio.martacos@gmail.com>
2022-07-22 13:12:47 -0700

    Task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		*head = aux->next;
 		if (aux->next != NULL)
-			aux->next->prev-> = NULL;
+			aux->next->prev = NULL;
 		free(aux);
 		return (1);
 	}
