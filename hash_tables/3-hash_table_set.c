a52c2b459a15990147a917198204a297e2792fb0
Imarino4 <ignacio.martacos@gmail.com>
2022-08-20 10:59:21 -0700

    Task 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to update
+ * @key_ the key, cannot be an empty string
+ * @value: value associated with the key, must be duplicated and can be empty
+ * Return: 1 in success, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_elem = NULL;
+	char *key_copy = NULL, *value_copy = NULL;
+
+	new_elem = malloc(sizeof(hash_node_t));
+	if (new_elem == NULL)
+	{
+		free(ht);
+		return (0);
+	}
+	key_copy = strdup(key);/*No puedo operar con const char*/
+	value_copy = strdup(value);
+	new_elem->key = key_copy;
+	new_elem->value = value_copy;
+	index = key_index((unsigned char *)key_copy, ht->size);/*casteo key*/
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new_elem;
+		new_elem->next = NULL;
+	}
+	else
+	{
+		new_elem->next = ht->array[index];
+		ht->array[index] = new_elem;
+	}
+	return (1);
+}
