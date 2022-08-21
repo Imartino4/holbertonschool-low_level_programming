03a7e4cc6a485a7df5937ac0567aa902d0bdc5b6
Imarino4 <ignacio.martacos@gmail.com>
2022-08-20 20:45:13 -0700

    remove actual node when key is the same

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,7 +5,7 @@
  * @key: the key, cannot be an empty string
  * @value: value associated with the key, must be duplicated and can be empty
  * Return: 1 in success, 0 otherwise.
- * Description: In case of collision the new element has the same key as the 
+ * Description: In case of collision the new element has the same key as the
  * actual, then remove the actual. If the keys are different, add the new
  * element at the beggining of the linked list
  */
